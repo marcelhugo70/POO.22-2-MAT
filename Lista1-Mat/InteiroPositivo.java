ae77cf3da93a99ef379edae6412a9f65c93b729c
Marcel Hugo <37884743+marcelhugo70@users.noreply.github.com>
2022-08-16 09:19:11 -0300

    Lista 1 - Exerc 5 (até item e)

--- Lista1-Mat/InteiroPositivo.java
+++ Lista1-Mat/InteiroPositivo.java
@@ -16,18 +16,54 @@ public class InteiroPositivo
             this.x = valor;
         }
     }
-    
+
     public int getValor(){
         return this.x;
     }
-    
+
     // item b
     public long multiplicar(InteiroPositivo outro){
         return this.x * outro.getValor();
     }
-    
+
     // item c
     public long fatorial(){
-        return 0;
+        long fat = 1;
+
+        for (int i=this.x; i > 1; i--){
+            fat = fat * i;
+        }
+
+        return fat;
+    }
+
+    // item d
+    public String identificarDivisores(){
+        String resultado = "Os divisores são ";
+        int contador = 0;
+        for (int den = 1; den <= this.x; den++){
+            if (this.x % den == 0){
+                resultado = resultado + den +", ";
+                contador++;
+            }
+        }
+        resultado = resultado + " e a quantidade de divisores é "+contador;
+        return resultado;
+    }
+
+    // item e
+    public int[] fibonacci(){
+        int[] vetor = new int[this.x];
+        // testes para evitar ArrayIndexOutOfBoundsException
+        if (vetor.length > 0){
+            vetor[0] = 1;
+            if (vetor.length > 1){
+                vetor[1] = 1;
+            }
+        }
+        for (int pos = 2; pos < this.x; pos++){
+            vetor[pos] = vetor[pos-1] + vetor[pos-2];
+        }
+        return vetor;
     }
 }
