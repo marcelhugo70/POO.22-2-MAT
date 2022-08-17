4fa9659d73f35671876c6fb5ee8722c80320f718
Marcel Hugo <37884743+marcelhugo70@users.noreply.github.com>
2022-08-17 11:08:36 -0300

    Lista2 - exerc 2 (início)

---
+++ Lista2M-Exerc2/src/Apresentacao.java
@@ -0,0 +1,76 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JTextArea;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Apresentacao {
+
+	private JFrame frame;
+	private JTextField tfFrase;
+	private JTextArea textArea;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Apresentacao window = new Apresentacao();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Apresentacao() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Digite uma frase:");
+		lblNewLabel.setBounds(10, 30, 106, 14);
+		frame.getContentPane().add(lblNewLabel);
+		
+		tfFrase = new JTextField();
+		tfFrase.setBounds(126, 27, 198, 20);
+		frame.getContentPane().add(tfFrase);
+		tfFrase.setColumns(10);
+		
+		JButton btnNewButton = new JButton("OK");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Frase f = new Frase(tfFrase.getText());
+				String[] vetor = f.decompor();
+				textArea.setText("Decomposição da frase\n");
+				for (int i=0; i < vetor.length; i++) {
+					textArea.append("\n"+vetor[i]+" - "+vetor[i].length());
+				}
+			}
+		});
+		btnNewButton.setBounds(334, 26, 89, 23);
+		frame.getContentPane().add(btnNewButton);
+		
+		textArea = new JTextArea();
+		textArea.setBounds(10, 78, 314, 172);
+		frame.getContentPane().add(textArea);
+	}
+}
---
+++ Lista2M-Exerc2/src/Frase.java
@@ -0,0 +1,16 @@
+
+public class Frase {
+	private String texto;
+
+	public Frase(String texto) {
+		super();
+		this.texto = texto;
+	}
+	
+	public String[] decompor() {
+		// to do
+		return null;
+	}
+	
+
+}
